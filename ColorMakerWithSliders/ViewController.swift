75f78aa31c3c92278fc89dd98533627c95726442
Surajit A Bose <surajit@udacity.com>
2015-06-25 22:28:57 -0700

    Initial Commit

---
+++ ColorMakerWithSliders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorMakerWithSliders
+//
+//  Created by Surajit A Bose on 6/25/15.
+//  Copyright (c) 2015 Surajit A Bose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
